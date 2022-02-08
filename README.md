7053f2ce4fc9d03bb177890a201bb6a3c14ba598
Wataru <bireleysaddiction@gmail.com>
2022-02-08 12:49:16 +0900

    Added UI including some sound effects and background image.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rock-paper-scissors
 Tackled the problem, Rock Paper Scissors, for the first time.
+
+Photo credit
+
+Photo by <a href="https://unsplash.com/@nordwood?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">NordWood Themes</a> on <a href="https://unsplash.com/s/photos/white?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,58 +1,120 @@
-function playerInput() {
-    return prompt(("Rock, Paper, Scissors?").toLowerCase());
-}
+const sound = new Audio("./sound/effect2.wav");
+const applause = new Audio("./sound/applause.wav")
 
-const rpcArray = ["rock", "paper", "scissors"]
+const rpcArray = ["Rock", "Paper", "Scissors"]
 
 function computerInput() {
-    return rpcArray[Math.floor(Math.random() * rpcArray.length)];
+  return rpcArray[Math.floor(Math.random() * rpcArray.length)];
 }
 
-// const computerSelection = computerInput();
-// const playerSelection = playerInput();
+// a whole game starts here
 
-let compWin = 0;
-let playerWin = 0;
+function gameStart() {
+  
+    let playerWin = 0;
+    let compWin = 0;
+  
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        sound.currentTime = 0;
+        sound.volume = 0.5;
+        sound.play();
+        const playerSelection = button.textContent;
+        const computerSelection = computerInput();
+        roundResultTextHolder.textContent = playRound(playerSelection, computerSelection);
+        playerWinTextHolder.textContent = "Player win count: " + playerWin;
+        computerWinTextHolder.textContent = "Computer win count: " + compWin;      
+        finishGame();
+        });
+    });
 
-function playRound(playerInput, computerInput) {
+    function playRound(playerSelection, computerSelection) {
+	    const draw = "It was draw!"
+        const win = "You won the round! Well done!"
+        const lose = "You lost the round."
+        if (playerSelection == computerSelection) {
+            return draw;
+        } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+            playerWin++;
+            console.log(playerWin);
+            return win;
+        } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+            compWin++;
+            console.log(compWin);
+ 	        return lose;
+        } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+            compWin++;
+            console.log(compWin);
+            return lose;
+        } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+            playerWin++;
+            console.log(playerWin);
+            return win;
+        } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+            playerWin++;
+            console.log(playerWin);
+            return win;
+        } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+            compWin++;
+            console.log(compWin);
+            return lose;
+        }
+    }
 
-    if (playerInput == computerInput) {
-        console.log("It was draw!");
-    } else if (playerInput == "rock" && computerInput == "scissors") {
-        console.log("You won the round!");
-        playerWin++;
-    } else if (playerInput == "rock" && computerInput == "paper") {
-        console.log("You lost the round.");
-        compWin++;
-    } else if (playerInput == "scissors" && computerInput == "rock") {
-        console.log("You lost the round.");
-        compWin++;
-    } else if (playerInput == "scissors" && computerInput == "paper") {
-        console.log("You won the round!");
-        playerWin++;
-    } else if (playerInput == "paper" && computerInput == "rock") {
-        console.log("You won the round!");
-        playerWin++;
-    } else if (playerInput == "paper" && computerInput == "scissors") {
-        console.log("You lost the round.");
-        compWin++;
+    function finishGame() {
+        if (playerWin == 5) {
+            gameResultTextHolder.textContent = "You are the winner! Here's your cup!"
+            applause.play();
+            document.getElementById("btn1").disabled = true;
+            document.getElementById("btn2").disabled = true;
+            document.getElementById("btn3").disabled = true;
+
+            const playMoreButton = document.createElement("button");
+            playMoreButton.textContent = ("Play More!");
+            playMoreButton.classList.add("play-more");
+            resultContainer.appendChild(playMoreButton);
+
+            playMoreButton.addEventListener("click", () => {
+                window.location.reload();
+            });
+
+        }   else if  (compWin == 5) {
+            gameResultTextHolder.textContent = "Sorry, our computer won the game."
+            document.getElementById("btn1").disabled = true;
+            document.getElementById("btn2").disabled = true;
+            document.getElementById("btn3").disabled = true;
+
+            const playMoreButton = document.createElement("button");
+            playMoreButton.textContent = ("Play More!");
+            playMoreButton.setAttribute("id", "btn4");
+            resultContainer.appendChild(playMoreButton);
+
+            playMoreButton.addEventListener("click", () => {
+                window.location.reload();
+            });
+        } 
     }
 }
 
-for (let i = 0; i < 5; i++) {
-    playRound(playerInput(), computerInput());
-}
+gameStart();
 
-console.log(`You won ${playerWin} times, and computer won ${compWin} times.`);
 
-function result() {
-    if (playerWin > compWin) {
-        console.log("You are the winner!");
-    }   else if (playerWin == compWin) {
-        console.log("It was draw! Thanks for playing!");
-    }   else  {
-        console.log("Computer won the game, sorry!");
-    }
-}
+const resultContainer = document.getElementById("result-container");
 
-result();
+const playerWinTextHolder = document.createElement('div');
+playerWinTextHolder.classList.add("playerwin");
+resultContainer.appendChild(playerWinTextHolder);
+
+const computerWinTextHolder = document.createElement('div');
+computerWinTextHolder.classList.add("computerwin");
+resultContainer.appendChild(computerWinTextHolder);
+
+const roundResultTextHolder = document.createElement('div');
+roundResultTextHolder.classList.add("round-result");
+resultContainer.appendChild(roundResultTextHolder);
+
+const gameResultTextHolder = document.createElement('h2');
+gameResultTextHolder.style.color = "brown";
+gameResultTextHolder.classList.add("game-result");
+resultContainer.appendChild(gameResultTextHolder);
